d3b4aaab8e9254b2ed8c92c3c0c0dabcef829e51
ArmoSchwarzenegger <wesleyhsieh1996@gmail.com>
2021-08-22 18:09:20 -0700

    create/delete channel

---
+++ bot.py
@@ -0,0 +1,40 @@
+import discord
+import random
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='!')
+
+@client.event
+async def on_ready():
+    print('Bot is ready!')
+
+@client.command()
+async def playWerewolf(ctx):
+    await ctx.send('Are you guys ready to play Werewolf game?')
+    guild = ctx.message.guild
+    channel = discord.utils.get(guild.text_channels, name="werewolf")
+    if not channel:
+        await guild.create_text_channel("werewolf")
+    if channel:
+        await ctx.send('There\'s already a channel!')
+        
+@client.command()
+async def endGame(ctx):
+    guild = ctx.message.guild
+    channel = discord.utils.get(guild.text_channels, name="werewolf")
+    if channel:
+        await channel.delete()
+    
+    
+# @client.command()
+# async def clear(ctx, amount=5):
+#     if amount == 0: return
+#     await ctx.channel.purge(limit=amount+1)
+    
+# Function for returning a random response to the user
+# @client.command(aliases=['8ball', 'test']) # The passing array are the tags can be used to call this fn
+# async def _8ball(ctx, *, question):
+#     responses = ['Yes', 'No', 'Maybe']
+#     await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+        
+client.run('ODc5MTQ2MDcxMDEwNDQ3Mzgw.YSLelQ.KVEC75VeDzACLcslcaO42VX6LKI')
