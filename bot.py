a0229864e2ae3325f42660ace1da2e8b3a61f1b0
AmoSchwarzenegger <43979742+AmoSchwarzenegger@users.noreply.github.com>
2021-08-23 10:23:03 -0700

    cog

--- bot.py
+++ bot.py
@@ -1,39 +1,22 @@
 import discord
-import random
+import os
 from discord.ext import commands
 
 client = commands.Bot(command_prefix='!')
 
-@client.event
-async def on_ready():
-    print('Bot is ready!')
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+    
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
 
-@client.command()
-async def playWerewolf(ctx):
-    await ctx.send('Are you guys ready to play Werewolf game?')
-    guild = ctx.message.guild
-    channel = discord.utils.get(guild.text_channels, name="werewolf")
-    if not channel:
-        await guild.create_text_channel("werewolf")
-    if channel:
-        await ctx.send('There\'s already a channel!')
-        
-@client.command()
-async def endGame(ctx):
-    guild = ctx.message.guild
-    channel = discord.utils.get(guild.text_channels, name="werewolf")
-    if channel:
-        await channel.delete()
-    
-    
-# @client.command()
-# async def clear(ctx, amount=5):
-#     if amount == 0: return
-#     await ctx.channel.purge(limit=amount+1)
-    
-# Function for returning a random response to the user
-# @client.command(aliases=['8ball', 'test']) # The passing array are the tags can be used to call this fn
-# async def _8ball(ctx, *, question):
-#     responses = ['Yes', 'No', 'Maybe']
-#     await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
-        
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
